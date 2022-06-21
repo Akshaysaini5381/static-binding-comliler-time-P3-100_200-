4184d9a354884ee961531a79ec4e22eb89e15036
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-22 02:19:14 +0530

    Add project files.

---
+++ static binding comliler time P3(100_200)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace static_binding_comliler_time_P3_100_200_
+{
+    class test
+    {
+        public int x = 100;
+
+    }
+    class Program:test
+    {
+        public  new int x = 200;
+        void show()
+        {
+            // base kewword parent class variable asses:
+            Console.WriteLine(base.x+" "+x);
+          /* Console.WriteLine(x+" "+x);*/
+
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.show();
+            // Out Put=100 200;
+            // static binding not posible
+            /*  test obj = new Program();
+              obj.show();*/
+            
+        }
+    }
+}
